abbe4cf9b0761be81d484d5a0516e044bee137ff
AnJiIn <aji0719@gmail.com>
2022-01-04 14:12:20 +0900

    8강

--- management/client/src/setupProxy.js
+++ management/client/src/setupProxy.js
@@ -2,9 +2,22 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware('/api', {
+    createProxyMiddleware(['/api', '/api/customers'], {
       target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
 };
+
+// const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// const app = express();
+
+// app.use('/api', 
+// createProxyMiddleware({ 
+//   target: 'http://localhost:5000', 
+//   changeOrigin: true 
+// })
+// );
+// app.listen(5000);
